5f098f4dd10405d1b0648738b315a271eb58c3ae
sameer6n <sameer6n@gmail.com>
2016-05-23 18:37:54 -0500

    Initial Checking of User Management tool.

---
+++ egen-be-challenge/src/main/java/com/sameer/myapp/egen_be_challenge/App.java
@@ -0,0 +1,13 @@
+package com.sameer.myapp.egen_be_challenge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
